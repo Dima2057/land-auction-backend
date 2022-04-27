59a0f309965b75c1eceea93e76f25c4abc2f51cb
Dmytro <dima.tumochko@gmail.com>
2022-04-27 20:52:25 +0300

    Add basic configs and project structure

---
+++ src/main/java/com/heroku/landx/LandxApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.landx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandxApplication.class, args);
+    }
+
+}
